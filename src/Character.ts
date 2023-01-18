51441730f2ed4b1859a17d6225ca80063e10d3c5
Mayara Marques <mayaramarquescosta@outlook.com>
2023-01-18 17:59:55 -0300

    chore: Criando a classe Character

---
+++ src/Character.ts
@@ -0,0 +1,82 @@
+import Fighter from './Fighter';
+import Energy from './Energy';
+import Archetype, { Mage } from './Archetypes';
+import Race, { Elf } from './Races';
+
+export default class Character implements Fighter {
+  race: Race;
+  archetype: Archetype;
+  maxLifePoints: number;
+
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._dexterity = Math.ceil(Math.random() * 9);
+    this.race = new Elf(name, this._dexterity);
+    this.archetype = new Mage(name);
+    this.maxLifePoints = this.race.maxLifePoints / 2;
+    this._lifePoints = this.maxLifePoints;
+    this._strength = Math.ceil(Math.random() * 9);
+    this._defense = Math.ceil(Math.random() * 9);
+    this._energy = { 
+      type_: this.archetype.energyType,
+      amount: Math.ceil(Math.random() * 9),
+    };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this.defense;
+    if (damage > 0) { this._lifePoints -= damage; }
+    if (damage <= 0) { this._lifePoints -= 1; }
+    if (this._lifePoints <= 0) { this._lifePoints = -1; }
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    const damage = this._strength;
+    enemy.receiveDamage(damage);
+  }
+
+  special(enemy: Fighter): void {
+    const damage = 2 * this._strength;
+    enemy.receiveDamage(damage);
+  }
+
+  levelUp(): void {
+    this.maxLifePoints += Math.ceil(Math.random() * 9);
+    if (this.maxLifePoints > this.race.maxLifePoints) { 
+      this.maxLifePoints = this.race.maxLifePoints; 
+    }
+    if (this._lifePoints < this.maxLifePoints) {
+      this._lifePoints = this.maxLifePoints;
+    }
+    this._strength += Math.ceil(Math.random() * 9);
+    this._dexterity += Math.ceil(Math.random() * 9);
+    this._defense += Math.ceil(Math.random() * 9);
+    this._energy.amount = 10;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  get defense() {
+    return this._strength;
+  }
+
+  get energy() {
+    return { ...this._energy };
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+}
